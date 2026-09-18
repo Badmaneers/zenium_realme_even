fef8aa56418ecf783386a193e62ffece5c622836
Badmaneers <dukebraham24@gmail.com>
2026-09-18 08:29:01 +0530

    Merge 4.19.325-cip-136
    Import the CIP linux-4.19.y-cip tree (merge of linux-4.19.y-st,
    suffix bumped to -cip136).

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -1686,6 +1686,7 @@ void cpufreq_suspend(void)
 	if (!cpufreq_driver)
 		return;
 
+	cpus_read_lock();
 	if (!has_target() && !cpufreq_driver->suspend)
 		goto suspend;
 
@@ -1705,6 +1706,7 @@ void cpufreq_suspend(void)
 
 suspend:
 	cpufreq_suspended = true;
+	cpus_read_unlock();
 }
 
 /**
